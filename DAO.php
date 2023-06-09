7baa71b9aee71059db3638f61d41cfef6f301821
Milica Sebić <92763213+zzomie@users.noreply.github.com>
2023-06-09 17:32:45 +0200

    Create DAO.php

---
+++ DAO.php
@@ -0,0 +1,37 @@
+<?php
+require_once 'db.php';
+
+class DAO {
+	private $db;
+
+	private $AVERAGEGOALS = "SELECT AVG(brgolova) FROM fudbaleri";
+
+	private $GETFUDBALER = "SELECT * FROM fudbaleri WHERE brojgolova>n";
+	
+	public function __construct()
+	{
+		$this->db = DB::createInstance();
+	}
+
+	public function getFudbaler($n)
+	{	
+		$statement = $this->db->prepare($this->GETFUDBALER);
+		$statement->bindValue(1, $n, PDO::PARAM_INT);	
+		$statement->execute();
+		$result = $statement->fetchAll();
+		return $result;
+	}
+
+	public function brojGolova($brgolova)
+	{
+		$statement = $this->db->prepare($this->AVERAGEGOALS);
+		$statement->bindValue(1, $brgolova);
+		if($statement->fetch()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+}
+?>
